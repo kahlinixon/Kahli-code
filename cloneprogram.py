0bbaa910237afd9ca025f9142a9372cc74916992
kahlinixon <144710357+kahlinixon@users.noreply.github.com>
2023-11-08 18:01:07 +0000

    Clone activity

--- cloneprogram.py
+++ cloneprogram.py
@@ -1,4 +1,9 @@
-food=['apple','onion','watermelon','green bean','pear']
+x=range(1,10,2)
+for n in x:
+    print(n) 
 
-for i in range(len(food)):
-  print(food[i])
+
+number_list= [1,2,3,4,5,6,7,8,9,10]
+
+1. coppy file 
+2.paste file to codespaces
